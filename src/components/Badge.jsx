dd29f8d1e36912a619dae490024e8ad9dbad32ba
Grant <bearlyfunctionalgaming@gmail.com>
2021-07-04 17:01:29 -0700

    icons working for badges

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -1,11 +1,40 @@
 import React from "react";
+import blender from "./img/blender.png";
+import csharp from "./img/c-sharp.png";
+import unity from "./img/unity.png";
+import photoshop from "./img/photoshop.png";
+import css from "./img/css.png";
+import html from "./img/html.png";
+import javascript from "./img/javascript.png";
+import nodejs from "./img/node-js.png";
+import react from "./img/react.png";
+import solidity from "./img/solidity.png";
+import unreal from "./img/unreal.png";
 
 function Badge(props){
-    let skillName = props.skill;
-    return(
-        <div className="badge">
-            {skillName}
-        </div>
-    )};
+
+    switch (props.skill){
+        case "blender":
+            return(
+                <div className="badge"><img src={blender} alt=""/></div>
+            );
+        case "csharp":
+            return(
+                <div className="badge"><img src={csharp} alt=""/></div>
+            );
+        case "unity":
+            return(
+                <div className="badge"><img src={unity} alt=""/></div>
+            );
+        case "photoshop":
+            return(
+                <div className="badge"><img src={photoshop} alt=""/></div>
+            );
+        default:
+            return(
+                <div className="badge">{props.skill}</div>
+            )
+    }
+}
 
 export default Badge;
---
+++ src/components/img/index.js
@@ -0,0 +1,8 @@
+const logos = {
+    blender: require('./blender.png').default,
+    csharp: require('./c-sharp.png').default,
+    unity: require('./unity.png').default,
+    photoshop: require('./photoshop.png').default
+}
+
+export default logos;
